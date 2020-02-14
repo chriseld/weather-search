320415840e58ef24676229be3d8b1b1b0fb111c4
Chris Eld <chris.eld95@gmail.com>
2020-02-13 21:19:27 -0500

    added function to throw an error message if no city is found

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,27 +11,32 @@ $("#find-city").on("click", function(event) {
         method: "GET"
     }).then(function(response) {
 
-        // Create variables
-        
-        var card = $("<div class='card'>");
-        var firstLine = "<p style='font-weight: bold; display: inline;'>" + JSON.parse(JSON.stringify(response.name)) + " (" + moment().format('YYYY/MM/DD') + ")";
-        var wicon = $("<img class='wicon' alt='" + response.weather[0].description + "' src='http://openweathermap.org/img/w/" + response.weather[0].icon + ".png'><br>")
-        var temperature = "Temperature: " + JSON.parse(JSON.stringify(response.main.temp)) + "&deg; F<br>";
-        var humidity = "Humidity: " + JSON.parse(JSON.stringify(response.main.humidity)) + "%<br>";
-        var windspeed = "Windspeed: " + JSON.parse(JSON.stringify(response.wind.speed)) + " MPH<br>";
+            // Create variables
+            
+            var card = $("<div class='card'>");
+            var firstLine = "<p style='font-weight: bold; display: inline;'>" + JSON.parse(JSON.stringify(response.name)) + " (" + moment().format('YYYY/MM/DD') + ")";
+            var wicon = $("<img class='wicon' alt='" + response.weather[0].description + "' src='http://openweathermap.org/img/w/" + response.weather[0].icon + ".png'><br>")
+            var temperature = "Temperature: " + JSON.parse(JSON.stringify(response.main.temp)) + "&deg; F<br>";
+            var humidity = "Humidity: " + JSON.parse(JSON.stringify(response.main.humidity)) + "%<br>";
+            var windspeed = "Windspeed: " + JSON.parse(JSON.stringify(response.wind.speed)) + " MPH<br>";
 
-        // Append variables
-        
-        card.append(firstLine);
-        card.append(wicon);
-        card.append(temperature);
-        card.append(humidity);
-        card.append(windspeed);
+            // Append variables
+            
+            card.append(firstLine);
+            card.append(wicon);
+            card.append(temperature);
+            card.append(humidity);
+            card.append(windspeed);
 
 
-        // Add new cards above the previous
-        
-        $("#cards").prepend(card);
+            // Add new cards above the previous
+            
+            $("#cards").prepend(card);
+
     });
 
+});
+
+$( document ).ajaxError(function() {
+    alert("City not found! Please try again."); // If response code is 404, throw an error message
 });
