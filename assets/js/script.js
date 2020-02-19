469a3a3582fbdef26180e3771ae35d4ea8e5aec8
Chris Eld <chris.eld95@gmail.com>
2020-02-19 12:43:51 -0500

    fixed queryurl to use https

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ $("#find-city").on("click", function(event) {
 
     var city = $("#city-input").val();
 
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=6267744aea8e18ec095542c8b808ca0a&units=imperial";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=6267744aea8e18ec095542c8b808ca0a&units=imperial";
 
     $.ajax({
         url: queryURL,
@@ -15,7 +15,7 @@ $("#find-city").on("click", function(event) {
             
             var card = $("<div class='card'>");
             var firstLine = "<p style='font-weight: bold; display: inline;'>" + JSON.parse(JSON.stringify(response.name)) + " (" + moment().format('YYYY/MM/DD') + ")";
-            var wicon = $("<img class='wicon' alt='" + response.weather[0].description + "' src='http://openweathermap.org/img/w/" + response.weather[0].icon + ".png'><br>")
+            var wicon = $("<img class='wicon' alt='" + response.weather[0].description + "' src='http://openweathermap.org/img/w/" + response.weather[0].icon + ".png'><br>");
             var temperature = "Temperature: " + JSON.parse(JSON.stringify(response.main.temp)) + "&deg; F<br>";
             var humidity = "Humidity: " + JSON.parse(JSON.stringify(response.main.humidity)) + "%<br>";
             var windspeed = "Windspeed: " + JSON.parse(JSON.stringify(response.wind.speed)) + " MPH<br>";
